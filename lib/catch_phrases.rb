5a0872431ea8deb2ca3f857f254c25838a2feb9c
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-08 04:02:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,8 +7,11 @@ def toadstool
   phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts phrase
 end
+
 def link
-  phrase =  "puts out 'It's Dangerous To Go Alone! Take This."
+  phrase = "puts out 'It's Dangerous To Go Alone! Take This."
   puts phrase 
+end
+  
 
   
