ae83be46ae3409451908ab54ce89ac9bfeec18d4
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-08 03:29:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,17 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "'Thank You Mario! But Our Princess Is In Another Castle!'"
   puts phrase
 end
 
 def toadstool
   puts status
-end
+endase
+    end
+
+#def link
+#  puts #{phrase}
+#end
+
+#def any_phrase
+ # puts #{phrase}
+  
