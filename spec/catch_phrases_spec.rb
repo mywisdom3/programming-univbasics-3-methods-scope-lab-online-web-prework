0a5d84ae51d939c12d4cd5bf56dc901dbd013036
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-08 04:02:16 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -16,6 +16,7 @@ end
 
 describe "link" do
   it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+    phrase =  "puts out 'It's Dangerous To Go Alone! Take This."
     expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
   end
 end
